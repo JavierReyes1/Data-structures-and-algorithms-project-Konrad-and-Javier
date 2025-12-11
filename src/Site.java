bf42ca89289b9b194ab586d7b95647a189d8d43c
JavierReyes1 <c00312829@setu.ie>
2025-12-11 21:29:57 +0000

    Created a new class called Site

--- src/Site.java
+++ src/Site.java
@@ -3,21 +3,24 @@ public class Site{
 	private String siteName;
 	private int xValue;
 	private int yValue;
-	private int distance;
 
-	public Site(char siteCode, String siteName, int xValue, int Distance){
+	public Site(char siteCode, String siteName, int xValue, int yValue){
 		this.siteCode = siteCode;
 		this.siteName = siteName;
 		this.xValue = xValue;
 		this.yValue = yValue;
-		this.distance = distance;
 	}
+
+	public char getSiteCode(){return siteCode;}
+	public String getSiteName(){return siteName;}
+	public int getXValue(){return xValue;}
+	public int getYValue(){return yValue;}
+
 	@Override
 	public String toString(){
 		return "\nSite Code: " + siteCode
 		+ "\nSite Name: "	+ siteName
 		+ "\nValue of x: " + xValue
-		+ "\nValue of y: " + yValue
-		+ "\nDistance: " + distance;
+		+ "\nValue of y: " + yValue;
 	}
 }
