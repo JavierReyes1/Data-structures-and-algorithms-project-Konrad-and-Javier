65cd5c661fccd93949a56d601be253fe1b1d879e
JavierReyes1 <c00312829@setu.ie>
2025-12-11 21:29:57 +0000

    Created a new class called Site

--- src/Tokyo.java
+++ src/Tokyo.java
@@ -51,27 +51,36 @@
 //private int[] xValues = {3, 4, 5, 8, 9, 6, 11};
 //private int[] yValues = {7, 1, 3, 8, 7, 2, 2};
 
-//private int[][] dist = new int[7][7];
 
 import java.util.Scanner;
 public class Tokyo{
 
-	private Site sites[] = new Sites[7];
+	private Site sites[] = new Site[7];
 	static Scanner sc = new Scanner(System.in);
+	private int[][] dist = new int[7][7];
 
 
 	public Tokyo(){
 		initMap();
+		initSites();
 	}
+
+	private void initSites(){
+	//public Site(char siteCode, String siteName, int xValue, int yValue){
+				sites[0] = new Site('A', "Shibuya Crossing", 3, 7);
+				sites[1] = new Site('B', "Meiji Shrine", 4, 1);
+				sites[2] = new Site('C', "Tokyo Tower", 5, 3);
+				sites[3] = new Site('D', "Akihabara", 8, 8);
+				sites[4] = new Site('E', "Senso-Ji Temple", 9, 7);
+				sites[5] = new Site('F', "Ueno Park", 6, 2);//Ueno Park
+				sites[6] = new Site('G', "Tokyo Skytree", 11, 2);//Tokyo skytree
+			}
+
 
 	public void start(){
 		menu();
 	}
-	private void displayTable(){
-		for(int i = 0; i < xValues.length ; i++){
-			System.out.format("");
-		}
-	}
+
 
 	private void initMap() {
 		dist[0][1] = 4; dist[1][0] = 4;
@@ -98,11 +107,10 @@ public class Tokyo{
 			choice = sc.nextInt();
 			System.out.println("----------------------------------------");
 			switch(choice){
-				case 1 -> doSearch();
-				case 2 -> doInsert();
-				case 3 -> doAllCons();
-				case 4 -> doClosest();
-				case 5 -> displayTable();
+				case 1 -> search();
+				case 2 -> insert();
+				case 3 -> allCons();
+				case 4 -> closest();
 				case 0 -> System.out.println("Exit the program");
 				default -> System.out.println("Enter a valid choice");
 			}
@@ -111,25 +119,21 @@ public class Tokyo{
 
 	//-------------------------------------------------------------------------------------------------//
 
-	private void doSearch(){
+	private void search() {
 		sc.nextLine();
 		System.out.print("Enter search index: ");
-		String searchIndex = sc.nextLine();
-		search(searchIndex);
-	}
-	private int search(String searchIndex) {
+		char searchIndex = sc.nextLine().charAt(0);
 		for (int i = 0; i < 7; i++) {
-			if (siteCode[i].equals(searchIndex) || siteName[i].equals(searchIndex)) {
-				System.out.println("Found " + siteCode[i] + "  " + siteName[i]);
-				System.out.println("Coordinates (" + xValues[i] + "" + yValues[i] + ")");
-				return i;
+			if(sites[i].getSiteCode() == searchIndex || sites[i].getSiteName().equals(searchIndex)){
+				System.out.println("Found " + sites[i].getSiteCode() + "  " + sites[i].getSiteName());
+				System.out.println("Coordinates (" + sites[i].getXValue() + ", " + sites[i].getYValue() + ")");
+			}else{
+				System.out.println("Site not found");
 			}
 		}
-		System.out.println("Site not found");
-		return -1;
 	}
 	//-------------------------------------------------------------------------------------------------//
-	private void doInsert(){
+	private void insert(){
 		sc.nextLine();
 		System.out.print("Enter x: ");
 		int i = sc.nextInt();
@@ -137,9 +141,6 @@ public class Tokyo{
 		int j = sc.nextInt();
 		System.out.print("Enter Distance: ");
 		int distance = sc.nextInt();
-		insert(i, j, distance);
-	}
-	private void insert(int i, int j, int distance) {
 		if (i >= 0 && i < 7 && j >= 0 && j < 7) {
 			dist[i][j] = distance;
 			dist[j][i] = distance;
@@ -148,45 +149,39 @@ public class Tokyo{
 	}
 	//-------------------------------------------------------------------------------------------------//
 
-	private void doAllCons(){
+	private void allCons(){
 		sc.nextLine();
 		System.out.print("Enter x: ");
 		int x = sc.nextInt();
-		allCons(x);
-	}
-	private void allCons(int i) {
-		if (i >= 0 && i < 7) {
-			System.out.println("Connections from " + siteName[i] + " ");
-			for (int j = 0; j < 7; j++) {
-				if (dist[i][j] > 0) {
-					System.out.println(siteName[i] + "  " + siteName[j] + ": " + dist[i][j] + "KM");
+		if (x >= 0 && x < 7) {
+			System.out.println("Connections from " + sites[x].getSiteName() + " ");
+			for (int y = 0; y < 7; y++) {
+				if (dist[x][y] > 0) {
+					System.out.println(sites[x].getSiteName()  + "  " + sites[y].getSiteName() + ": " + dist[x][y] + "KM");
 				}
 			}
 		}
 	}
 	//-------------------------------------------------------------------------------------------------//
-	private void doClosest(){
+	private void closest(){
 		sc.nextLine();
 		System.out.print("Enter x: ");
 		int x = sc.nextInt();
-		closest(x);
-	}
-	private int closest(int i) {
-		if (i >= 0 && i < 7) {
+		if (x >= 0 && x < 7) {
 			int minDistance = 999;
 			int closestIndex = -1;
-			for (int j = 0; j < 7; j++) {
-				if (dist[i][j] > 0 && dist[i][j] < minDistance) {
-					minDistance = dist[i][j];
-					closestIndex = j;
+			for (int y = 0; y < 7; y++) {
+				if (dist[x][y] > 0 && dist[x][y] < minDistance) {
+					minDistance = dist[x][y];
+					closestIndex = y;
 				}
 			}
 			if (closestIndex != -1) {
-				System.out.println("Closest " + siteName[closestIndex] + " at the distance of " + minDistance + "KM");
+				System.out.println("Closest " + sites[closestIndex].getSiteName() + " at the distance of " + minDistance + "KM");
+			}else{
+				System.out.println("Not found");
 			}
-			return closestIndex;
 		}
-		return -1;
 	}
 }//end class
 
